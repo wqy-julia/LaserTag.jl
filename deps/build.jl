9aa8c20fa618bfc3607f8d2c85adb077ddc54313
WangQY17 <wqy17nju@163.com>
2022-02-26 23:06:01 +0800

    no deps

--- deps/build.jl
+++ deps/build.jl
@@ -4,12 +4,12 @@ catch ex
     @warn("Unable to run `lualatex -v`. Lualatex is required for visualizing LaserTag.")
     if Sys.islinux()
         @info("Attempting to install lualatex with apt-get.")
-        # cmds = [`sudo apt-get install texlive-latex-base`, `sudo apt-get install texlive-binaries`]
-        cmds = [`sudo apt-get install texlive-latex-extra`]
-        for c in cmds
-            @info("Running $c")
-            run(c)
-        end
+        # # cmds = [`sudo apt-get install texlive-latex-base`, `sudo apt-get install texlive-binaries`]
+        # cmds = [`sudo apt-get install texlive-latex-extra`]
+        # for c in cmds
+        #     @info("Running $c")
+        #     run(c)
+        # end
     end
 end
 
@@ -18,9 +18,9 @@ try
 catch
     @warn("Unable to run `which pdf2svg`. pdf2svg is required for visualizing LaserTag.")
     if Sys.islinux()
-        @info("Attempting to install pdf2svg with apt-get")
-        cmd = `sudo apt-get install pdf2svg`
-        @info("Running $cmd")
-        run(cmd)
+        # @info("Attempting to install pdf2svg with apt-get")
+        # cmd = `sudo apt-get install pdf2svg`
+        # @info("Running $cmd")
+        # run(cmd)
     end
 end
